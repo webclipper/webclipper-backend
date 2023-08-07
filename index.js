c41c7c31423af5faf3493fcba2ec34b2f069e3b2
DiamondYuan <admin@diamondyuan.com>
2023-08-07 21:01:56 +0800

    feat: support oauth

--- index.js
+++ index.js
@@ -1,9 +1,77 @@
+import Router from "@koa/router";
 import Koa from "koa";
+import * as qs from "qs";
+import fetch from "node-fetch";
+
+const config = {
+  port: process.env.PORT || 3000,
+  yuqueClientId: process.env.YUQUE_CLIENT_ID,
+  yuqueClientSecret: process.env.YUQUE_CLIENT_SECRET,
+  oneNoteClientId: process.env.ONENOTE_CLIENT_ID,
+  oneNoteRedirectUrl: process.env.ONENOTE_REDIRECT_URL,
+};
+
+console.log("port : ", !!config.port);
+console.log("yuqueClientId : ", !!config.yuqueClientId);
+console.log("yuqueClientSecret : ", !!config.yuqueClientSecret);
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World";
+function redirect(ctx, state, query) {
+  const isFirefox = ctx.headers["user-agent"]?.includes("Firefox");
+  if (isFirefox) {
+    ctx.set("Location", `moz-extension://${state}/tool.html#/auth?${query}`);
+  } else {
+    ctx.set("Location", `chrome-extension://${state}/tool.html#/auth?${query}`);
+  }
+  ctx.status = 302;
+}
+
+router.get("/yuque_oauth", async (ctx) => {
+  const { code, state } = ctx.query;
+  const response = fetch("https://www.yuque.com/oauth2/token", {
+    method: "POST",
+    body: JSON.stringify({
+      client_id: config.yuqueClientId,
+      client_secret: config.yuqueClientSecret,
+      code,
+      grant_type: "authorization_code",
+    }),
+  });
+  redirect(ctx, state, `${qs.stringify(response.data)}`);
 });
 
+router.get("/onenote_oauth", async (ctx) => {
+  const { code, state } = ctx.query;
+  const response = fetch(
+    "https://login.microsoftonline.com/common/oauth2/v2.0/token",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        client_id: config.oneNoteClientId,
+        grant_type: "authorization_code",
+        scope: "Notes.Create User.Read offline_access",
+        redirect_uri: config.oneNoteRedirectUrl,
+        code,
+      }),
+    }
+  );
+  redirect(
+    ctx,
+    state,
+    `${qs.stringify({
+      access_token: response.data.access_token,
+      type: "onenote_oauth",
+      refresh_token: response.data.refresh_token,
+    })}`
+  );
+});
+
+router.get("/ping", async () => {
+  ctx.status = 200;
+  ctx.body = `pong ${Date.now()}`;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 app.listen(3000);
